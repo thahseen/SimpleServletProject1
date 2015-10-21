e0263c66551eb26d15b9e717a1060eb43f31b6c3
Raseel Mohamed <raseel.mohamed@rmohamed-2.local>
2015-10-21 00:01:11 -0400

    This is the better version that from the master. I have this here so that We can always keep the master in the best and greatest form

--- src/org/thahseen/javabrains/SimpleServlet.java
+++ src/org/thahseen/javabrains/SimpleServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/SimpleServlet")
+@WebServlet(description= "/SimpleServlet", urlPatterns= {"/SimpleServletProject1"})
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
